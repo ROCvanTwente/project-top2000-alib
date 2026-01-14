b3aea061a4ee4904b95e6c70f3b7f460709515e6
Sunny Lim <0356150@student.rocvantwente.nl>
2026-01-14 11:52:27 +0100

    Artist API af

---
+++ TemplateJwtProject/Controllers/ArtistController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TemplateJwtProject.Models;
+using TemplateJwtProject.Models.DTOs;
+
+namespace TemplateJwtProject.Controllers
+{
+    [ApiController]
+    [Route("artists")]
+    public class ArtistController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public ArtistController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetArtists()
+        {
+            var result = _db.Top2000Entry
+                .Select(e => new
+                {
+                    ArtistName = e.Songs.Artist.Name,
+                    SongTitle = e.Songs.Titel,
+                    // evt. extra: ReleaseYear = e.Songs.ReleaseYear
+                })
+                .GroupBy(x => x.ArtistName.ToLower())
+                .Select(g => new ArtistTop2000Dto
+                {
+                    Name = g.First().ArtistName,
+
+                    // Unieke liedjes op basis van titel (niet SongId)
+                    SongsInTop2000 = g
+                        .Select(x => x.SongTitle.ToLower().Trim())
+                        .Distinct()
+                        .Count()
+                })
+                .OrderByDescending(a => a.SongsInTop2000)
+                .ToList();
+
+            if (result.Count == 0) return NotFound();
+            return Ok(result);
+        }
+    }
+}
---
+++ TemplateJwtProject/Models/DTOs/AristTop2000Dto.cs
@@ -0,0 +1,8 @@
+﻿namespace TemplateJwtProject.Models.DTOs
+{
+    public class ArtistTop2000Dto
+    {
+        public string? Name { get; set; }
+        public int SongsInTop2000 { get; set; }
+    }
+}
